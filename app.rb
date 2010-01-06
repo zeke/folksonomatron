b20e6db4c1a79e35d09bd6776c14b7b59cbd737d
Zeke Sikelianos <zeke@sikelianos.com>
2010-01-06 12:29:37 -0800

    Added some new views and a stylesheet

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 # %w{rubygems sinatra hpricot mechanize}.each {|lib| require lib}
 
 get '/' do
+  haml :index
+end
+
+get '/munge/*' do
   agent = WWW::Mechanize.new
-  page = agent.get "http://del.icio.us/url/check?show=notes_only&url=#{params[:url]}"
+  page = agent.get "http://del.icio.us/url/check?show=notes_only&url=#{params[:splat]}"
   doc = Hpricot(page.parser.to_s)
-  tags = (doc/"a.showTag").map {|a| a.attributes['href'].split("/").last }
-  tags.join(", ")
+  @tags = (doc/"a.showTag").map {|a| a.attributes['href'].split("/").last }
+  haml :munge
+end
+
+get '/stylesheets/stylesheet.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :stylesheet
 end
