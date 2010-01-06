1bbb020c67e17431971931adc9ab1b1c8fe6ded3
Zeke Sikelianos <zeke@sikelianos.com>
2010-01-06 12:39:21 -0800

    Added a simple form

--- app.rb
+++ app.rb
@@ -4,9 +4,15 @@ get '/' do
   haml :index
 end
 
+# Take POSTed form and turn it into a clean GET url..
+post '/mungepost' do
+  redirect "/munge/#{params[:url]}" 
+end
+
 get '/munge/*' do
   agent = WWW::Mechanize.new
-  page = agent.get "http://del.icio.us/url/check?show=notes_only&url=#{params[:splat]}"
+  @url = params[:splat]
+  page = agent.get "http://del.icio.us/url/check?show=notes_only&url=#{@url}"
   doc = Hpricot(page.parser.to_s)
   @tags = (doc/"a.showTag").map {|a| a.attributes['href'].split("/").last }
   haml :munge
